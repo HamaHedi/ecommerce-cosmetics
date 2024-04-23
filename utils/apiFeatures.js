8cd47a408c3c7391fb0c979e32e3ddaf930b82c2
HamaHedi <mohamedbenjedida18@gmail.com>
2024-04-23 14:42:56 -0700

    added fixed categori filter

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -7,31 +7,37 @@ class APIFeatures {
 	search() {
 		const keyword = this.queryStr.keyword
 			? {
-					name: {
-						$regex: this.queryStr.keyword,
-						$options: 'i',
-					},
-			  }
+				$or: [
+					{ name: { $regex: this.queryStr.keyword, $options: 'i' } },
+					{ category: { $regex: this.queryStr.keyword, $options: 'i' } }
+				]
+			}
 			: {}
-
+	
 		this.query = this.query.find({ ...keyword })
 		return this
 	}
-
+	
 	filter() {
 		const queryCopy = { ...this.queryStr }
-
-		// Removing fields from the query
+	
+		// Remove fields from the query
 		const removeFields = ['keyword', 'limit', 'page']
 		removeFields.forEach((el) => delete queryCopy[el])
-
-		// Advance filter for price, ratings etc
+	
+		// Handle category filtering
+		if (queryCopy.category) {
+			this.query = this.query.find({ category: queryCopy.category })
+		}
+	
+		// Advanced filter for price, ratings, etc.
 		let queryStr = JSON.stringify(queryCopy)
 		queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`)
-
+	
 		this.query = this.query.find(JSON.parse(queryStr))
 		return this
 	}
+	
 
 	pagination(resPerPage) {
 		const currentPage = Number(this.queryStr.page) || 1
