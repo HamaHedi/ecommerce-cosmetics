c1b0118e8ed7d6beaec0d39ab7b4a71c87852164
Hama <mohamedbenjedida18@gmail.com>
2025-01-04 00:17:56 +0100

    added color and size to order schema

--- models/order.js
+++ models/order.js
@@ -47,6 +47,14 @@ const orderSchema = mongoose.Schema(
 					type: Number,
 					required: true,
 				},
+				color: {
+					type: String,
+					required: false,
+				},
+				size: {
+					type: String,
+					required: false,
+				},
 				product: {
 					type: mongoose.Schema.Types.ObjectId,
 					required: true,
